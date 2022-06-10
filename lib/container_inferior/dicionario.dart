6f7590228575b9dc5b176c90d3107cd9223fd852
maria <mariahelenarocha54@gmail.com>
2022-06-10 15:16:23 -0300

    Pasta tabela dicionario

---
+++ lib/container_inferior/dicionario.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TabelaDicionario extends StatefulWidget {
+  const TabelaDicionario({Key? key}) : super(key: key);
+
+  @override
+  State<TabelaDicionario> createState() => _TabelaDicionarioState();
+}
+
+class _TabelaDicionarioState extends State<TabelaDicionario> {
+  @override
+  Widget build(BuildContext context) {
+    return ContainerDicionario();
+  }
+}
+
+class ContainerDicionario extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      color: Colors.black,
+    );
+  }
+}
