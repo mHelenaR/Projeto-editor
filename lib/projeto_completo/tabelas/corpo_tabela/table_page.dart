4ff22d8c15e538fd79f2e81566aec61051c7dfec
maria <mariahelenarocha54@gmail.com>
2022-07-19 17:41:14 -0300

    tabelas  linhas erradas

--- lib/projeto_completo/tabelas/corpo_tabela/table_page.dart
+++ lib/projeto_completo/tabelas/corpo_tabela/table_page.dart
@@ -147,20 +147,77 @@ class _ArquivoPaginaState extends State<ArquivoPagina> {
     //----------- TIT------------//
 
     //----------- CPO------------//
-    // for (int i = 1; i < _linhasTIT.length; i++) {
-    //   if (_linhasTIT[i] != '') {
-    //     String testeP = _linhasTIT[i];
-    //     String? testeO = testeP.split('CPO ').toString();
-    //     teste1 = [testeO];
-    //     teste3 = teste3 + teste1;
-    //     for (int p = 0; p < teste3.length; p++) {
-    //       teste2 = teste3[p].split('^');
-    //       for (int m = 0; m < teste3.length; m++) {
-    //         teste4[p] = teste2[m];
-    //       }
-    //     }
-    //   }
-    // }
+    for (int i = 1; i < _linhasTIT.length; i++) {
+      if (_linhasTIT[i] != '') {
+        String testeP = _linhasTIT[i];
+        String? testeO = testeP.split('CPO ').toString();
+        teste1 = [testeO];
+        teste3 = teste3 + teste1;
+
+        for (int p = 0; p < teste3.length; p++) {
+          teste2 = teste3[p].split('^');
+          teste4[p] = teste3[p];
+          // for (int n = 0; n < teste3.length; n++) {
+          //   }
+
+        }
+
+        rows = [
+          for (int rowTam = 0; rowTam < teste3.length; rowTam++) ...{
+            //linha
+
+            PlutoRow(
+              cells: {
+                //for (int rowTam = 0; rowTam < teste2.length; rowTam++) ...{
+                for (int rColTam = 0; rColTam < teste2.length; rColTam++) ...{
+                  //coluna
+
+                  rColTam.toString(): PlutoCell(value: /*'$rColTam|$rowTam'*/ ' ${teste2[rColTam]}'),
+                },
+                //}
+              },
+            ),
+          }
+        ];
+      }
+    }
+    print(teste4);
+    //backup
+/* for (int i = 1; i < _linhasTIT.length; i++) {
+      if (_linhasTIT[i] != '') {
+        String testeP = _linhasTIT[i];
+        String? testeO = testeP.split('CPO ').toString();
+        teste1 = [testeO];
+        teste3 = teste3 + teste1;
+        for (int p = 0; p < teste3.length; p++) {
+          teste2 = teste3[p].split('^');
+        }
+      for (int m = 0; m < teste3.length; m++) {
+      for (int n = 0; n < teste2.length; n++) {
+        //coluna
+        teste4[m] = teste2[n];
+        // print(teste2[rColTam]);
+      }
+    }
+        rows = [
+          for (int rowTam = 0; rowTam < teste3.length; rowTam++) ...{
+            //linha
+
+            PlutoRow(
+              cells: {
+                //for (int rowTam = 0; rowTam < teste2.length; rowTam++) ...{
+                for (int rColTam = 0; rColTam < teste2.length; rColTam++) ...{
+                  //coluna
+
+                  rColTam.toString(): PlutoCell(value: /*'$rColTam|$rowTam'*/ ' ${teste2[rColTam]}'),
+                },
+                //}
+              },
+            ),
+          }
+        ];
+      }
+    } */
 
     //arrumar colunas com tamanhos iguais
     columns = <PlutoColumn>[
@@ -168,27 +225,13 @@ class _ArquivoPaginaState extends State<ArquivoPagina> {
         //coluna
 
         PlutoColumn(
-          title: 'Coluna - $colTam| ${_arrayString[colTam]}',
+          title: ' $colTam|  ${_arrayString[colTam]}',
           field: colTam.toString(),
           type: PlutoColumnType.text(),
         ),
       }
     ];
-    rows = [
-      for (int rowTam = 0; rowTam < _arrayString.length; rowTam++) ...{
-        //linha
 
-        PlutoRow(
-          cells: {
-            for (int rColTam = 0; rColTam < _arrayString.length; rColTam++) ...{
-              //coluna
-
-              rColTam.toString(): PlutoCell(value: 'Col - $rColTam | Row - $rowTam'),
-            },
-          },
-        ),
-      }
-    ];
     //columns: columns,
     // [
     //   for (int j = 0; j < _arrayString.length; j++) ...{
@@ -199,7 +242,6 @@ class _ArquivoPaginaState extends State<ArquivoPagina> {
     //     ),
     //   }
     // ],
-
     // rows: rows,
     // [
     //   for (int i = 0; i < teste3.length; i++) ...{
@@ -212,10 +254,8 @@ class _ArquivoPaginaState extends State<ArquivoPagina> {
     //     ),
     //   }
     // ],
-
     //print(teste4);
     // print(teste4);
-
     //   if (posCPO == 0) {
     //     linhaCPO = [_linhasTIT[i].substring(0, 3)];
     //   }
