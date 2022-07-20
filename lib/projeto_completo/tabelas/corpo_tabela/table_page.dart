c5c47f891e6a9484b2ccf25ec42d11b183e31ace
maria <mariahelenarocha54@gmail.com>
2022-07-20 17:40:09 -0300

    arrumar função nome das tabelas

--- lib/projeto_completo/tabelas/corpo_tabela/table_page.dart
+++ lib/projeto_completo/tabelas/corpo_tabela/table_page.dart
@@ -119,180 +119,95 @@ class _ArquivoPaginaState extends State<ArquivoPagina> {
   List<String> teste1 = [];
   List<String> teste2 = [];
   List<String> teste3 = [];
+  List<String> teste5 = [];
+  List<String> _linhasTIT = [];
+
   List<PlutoRow> rows = [];
   List<PlutoColumn> columns = [];
-  Map<int, String> teste4 = Map();
-  List<String> teste5 = [];
-  Map<dynamic, dynamic> teste6 = Map();
-  List<List<String>> teste7 = [];
 
   colunasTabelasArquivo() async {
     // espera o carregamento da variavel
     _separarArquivo = await conteudoArquivo;
 
     // cria lista com cpo
-    separaTabelasArquivo = _separarArquivo.split('TIT ');
-    List<String> _linhasTIT = separaTabelasArquivo[1].split('\r\n');
+    //separaTabelasArquivo = _separarArquivo.split('TIT ');
+    separaTabelasArquivo = _separarArquivo.split('\r\n');
+    for (int i = 0; i < separaTabelasArquivo.length; i++) {
+      // String recebe = separaTabelasArquivo[i].split('\r\n').toString();
+      String nova = separaTabelasArquivo[i];
+      print(nova);
+      // if (nova.substring(0, 3) == 'TIT') {
+      //   int posCharacterArquivo = separaTabelasArquivo[i].indexOf('#') + 1;
+      //   String recebe = separaTabelasArquivo[i].substring(posCharacterArquivo);
+      //   nomeColSeparada.add('teste');
+      //   print(nomeColSeparada);
+      // }
 
-    //----------- TIT------------//
+      // int posCharacterArquivo = recebe.indexOf('#') + 1;
+      // int posC = recebe.indexOf(', ') - 1;
+      // String recebendo = recebe.substring(posCharacterArquivo);
 
+      // String recebe = separaTabelasArquivo[i].split('\r\n').toString();
+      // _linhasTIT.add(recebe);
+      //print(_linhasTIT);
+    }
+
+    // for (int j = 0; j < _linhasTIT.length; j++) {
+    //   int posCharacterArquivo = _linhasTIT[j].indexOf('#') + 1;
+    //   String recebe = _linhasTIT[j].substring(posCharacterArquivo);
+    //   nomeColSeparada.add(recebe);
+    //   //print(nomeColSeparada);
+
+    // }
+    // for (int m = 0; m < nomeColSeparada.length; m++) {
+    //   nomeColunas = nomeColSeparada[m].split('|');
+    //   print(nomeColunas);
+    // }
+
+    //----------- TIT-INICIO-----------//
     //encontra posição do caracter para extrair
-    int posCharacterArquivo = _linhasTIT[0].indexOf('#') + 1;
-    nomeColSeparada = [_linhasTIT[0].substring(posCharacterArquivo)];
-
+    // int posCharacterArquivo = _linhasTIT[0].indexOf('#') + 1;
+    // nomeColSeparada = [_linhasTIT[0].substring(posCharacterArquivo)];
     // retira o separador
-    nomeColunas = nomeColSeparada[0].split('|');
+    // nomeColunas = nomeColSeparada[0].split('|');
 
-    setState(() {
-      _arrayString = nomeColunas;
-    });
+    // setState(() {
+    //   _arrayString = nomeColunas;
+    // });
+    //----------- TIT-FIM-----------//
 
-    //----------- TIT------------//
-
-    //----------- CPO------------//
-    for (int i = 1; i < _linhasTIT.length; i++) {
-      if (_linhasTIT[i] != '') {
-        String testeP = _linhasTIT[i];
-        String? testeO = testeP.split('CPO ').toString();
-        teste1 = [testeO];
-        teste3 = teste3 + teste1;
-
-        for (int p = 0; p < teste3.length; p++) {
-          teste2 = teste3[p].split('^');
-          // teste4[p] = teste3[p];
-          for (int n = 0; n < teste2.length; n++) {
-            teste4[n] = teste2[n];
-            //teste5 = teste4[n];
-            // teste7 = [
-            //   [p.toString()],
-            //   [teste2[n]]
-            // ];
-            // print(teste7);
-
-            // rows = [P
-            //   for (int rowTam = 0; rowTam < teste3.length; rowTam++) ...{
-            //     //linha
-            //     PlutoRow(
-            //       cells: {
-            //         //for (int rowTam = 0; rowTam < teste2.length; rowTam++) ...{
-            //         for (int rColTam = 0; rColTam < teste4.length; rColTam++) ...{
-            //           //coluna
-            //           rColTam.toString(): PlutoCell(
-            //               value: Container(
-            //             child: Center(
-            //               child: Text('{$teste4[rColTam]}'),
-            //             ),
-            //           )),
-            //         },
-            //         //}
-            //       },
-            //     ),
-            //   }
-            // ];
-          }
-        }
-        // for (int p = 0; p < teste3.length; p++) {
-        //   teste6[p] = [teste3];
-        // }
-        // print(teste6);
-        // print(teste4.length);
-        // print(teste2.length);
-
-        rows.addAll(
-          [
-            PlutoRow(
-              cells: {
-                for (int rColTam = 0; rColTam < teste4.length; rColTam++) ...{
-                  rColTam.toString(): PlutoCell(value: teste2[rColTam]),
-                },
-              },
-            ),
-          ],
-        );
-      }
-    }
-
-    //backup
-/* for (int i = 1; i < _linhasTIT.length; i++) {
-      if (_linhasTIT[i] != '') {
-        String testeP = _linhasTIT[i];
-        String? testeO = testeP.split('CPO ').toString();
-        teste1 = [testeO];
-        teste3 = teste3 + teste1;
-        for (int p = 0; p < teste3.length; p++) {
-          teste2 = teste3[p].split('^');
-        }
-      for (int m = 0; m < teste3.length; m++) {
-      for (int n = 0; n < teste2.length; n++) {
-        //coluna
-        teste4[m] = teste2[n];
-        // print(teste2[rColTam]);
-      }
-    }
-        rows = [
-          for (int rowTam = 0; rowTam < teste3.length; rowTam++) ...{
-            //linha
-
-            PlutoRow(
-              cells: {
-                //for (int rowTam = 0; rowTam < teste2.length; rowTam++) ...{
-                for (int rColTam = 0; rColTam < teste2.length; rColTam++) ...{
-                  //coluna
-
-                  rColTam.toString(): PlutoCell(value: /*'$rColTam|$rowTam'*/ ' ${teste2[rColTam]}'),
-                },
-                //}
-              },
-            ),
-          }
-        ];
-      }
-    } */
-
-    //arrumar colunas com tamanhos iguais
-    columns = <PlutoColumn>[
-      for (int colTam = 0; colTam < _arrayString.length; colTam++) ...{
-        //coluna
-        PlutoColumn(
-          title: '$colTam|${_arrayString[colTam]}',
-          field: colTam.toString(),
-          type: PlutoColumnType.text(),
-        ),
-      }
-    ];
-
-    //columns: columns,
-    // [
-    //   for (int j = 0; j < _arrayString.length; j++) ...{
+    //---------- CPO-INICIO---------//
+    // for (int i = 1; i < _linhasTIT.length; i++) {
+    //   if (_linhasTIT[i] != '') {
+    //     String testeP = _linhasTIT[i];
+    //     String? testeO = testeP.split('CPO ').toString();
+    //     teste1 = [testeO];
+    //     teste3 = teste3 + teste1;
+    //     for (int p = 0; p < teste3.length; p++) {
+    //       teste2 = teste3[p].split('^');
+    //     }
+    //     rows.addAll([
+    //       PlutoRow(
+    //         cells: {
+    //           for (int rowTam = 0; rowTam < teste2.length; rowTam++) ...{
+    //             rowTam.toString(): PlutoCell(value: teste2[rowTam]),
+    //           },
+    //         },
+    //       ),
+    //     ]);
+    //   }
+    // }
+    // columns = <PlutoColumn>[
+    //   for (int colTam = 0; colTam < _arrayString.length; colTam++) ...{
+    //     //coluna
     //     PlutoColumn(
-    //       title: _arrayString[j],
-    //       field: j.toString(),
+    //       title: '$colTam|${_arrayString[colTam]}',
+    //       field: colTam.toString(),
     //       type: PlutoColumnType.text(),
     //     ),
     //   }
-    // ],
-    // rows: rows,
-    // [
-    //   for (int i = 0; i < teste3.length; i++) ...{
-    //     PlutoRow(
-    //       cells: {
-    //         for (int j = 0; j < teste2.length; j++) ...{
-    //           j.toString(): PlutoCell(value: j.toString() + '  ' + i.toString() + '  ' + teste2[j]),
-    //         },
-    //       },
-    //     ),
-    //   }
-    // ],
-    //print(teste4);
-    // print(teste4);
-    //   if (posCPO == 0) {
-    //     linhaCPO = [_linhasTIT[i].substring(0, 3)];
-    //   }
-    //   teste1 = teste1 + linhaCPO;
-    // }
-    // print(teste1);
-
-    //----------- CPO------------//
+    // ];
+    //----------- CPO-FIM-----------//
   }
 
   nomeTabelasArquivo() async {
